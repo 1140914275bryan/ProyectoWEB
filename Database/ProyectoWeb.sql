5aec13f8aeace5a65b5b8ff9ac032f8b174cc3e7
1140914275bryan <bryanbohorquez14@gmail.com>
2025-09-25 21:37:32 -0500

    Update and rename ProyectoWeb.sql to Database/ProyectoWeb.sql

---
+++ Database/ProyectoWeb.sql
@@ -0,0 +1,359 @@
+﻿USE [master]
+GO
+/****** Object:  Database [DBVENTA]    Script Date: 25/09/2025 9:28:58 p. m. ******/
+CREATE DATABASE [DBVENTA]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'DBVENTA', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\DBVENTA.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'DBVENTA_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\DBVENTA_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
+GO
+ALTER DATABASE [DBVENTA] SET COMPATIBILITY_LEVEL = 160
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [DBVENTA].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [DBVENTA] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [DBVENTA] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [DBVENTA] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [DBVENTA] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [DBVENTA] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [DBVENTA] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [DBVENTA] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [DBVENTA] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [DBVENTA] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [DBVENTA] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [DBVENTA] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [DBVENTA] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [DBVENTA] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [DBVENTA] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [DBVENTA] SET  ENABLE_BROKER 
+GO
+ALTER DATABASE [DBVENTA] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [DBVENTA] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [DBVENTA] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [DBVENTA] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [DBVENTA] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [DBVENTA] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [DBVENTA] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [DBVENTA] SET RECOVERY FULL 
+GO
+ALTER DATABASE [DBVENTA] SET  MULTI_USER 
+GO
+ALTER DATABASE [DBVENTA] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [DBVENTA] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [DBVENTA] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [DBVENTA] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [DBVENTA] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [DBVENTA] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+EXEC sys.sp_db_vardecimal_storage_format N'DBVENTA', N'ON'
+GO
+ALTER DATABASE [DBVENTA] SET QUERY_STORE = ON
+GO
+ALTER DATABASE [DBVENTA] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
+GO
+USE [DBVENTA]
+GO
+/****** Object:  User [Administrator]    Script Date: 25/09/2025 9:28:59 p. m. ******/
+CREATE USER [Administrator] FOR LOGIN [Administrator] WITH DEFAULT_SCHEMA=[dbo]
+GO
+/****** Object:  User [Admin]    Script Date: 25/09/2025 9:28:59 p. m. ******/
+CREATE USER [Admin] FOR LOGIN [Admin] WITH DEFAULT_SCHEMA=[dbo]
+GO
+/****** Object:  Table [dbo].[Categoria]    Script Date: 25/09/2025 9:28:59 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Categoria](
+	[idCategoria] [int] IDENTITY(1,1) NOT NULL,
+	[descripcion] [varchar](50) NULL,
+	[esActivo] [bit] NULL,
+	[fechaRegistro] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idCategoria] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Configuracion]    Script Date: 25/09/2025 9:28:59 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Configuracion](
+	[recurso] [varchar](50) NULL,
+	[propiedad] [varchar](50) NULL,
+	[valor] [varchar](max) NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[DetalleVenta]    Script Date: 25/09/2025 9:28:59 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[DetalleVenta](
+	[idDetalleVenta] [int] IDENTITY(1,1) NOT NULL,
+	[idVenta] [int] NULL,
+	[idProducto] [int] NULL,
+	[marcaProducto] [varchar](100) NULL,
+	[descripcionProducto] [varchar](100) NULL,
+	[categoriaProducto] [varchar](100) NULL,
+	[cantidad] [int] NULL,
+	[precio] [decimal](10, 2) NULL,
+	[total] [decimal](10, 2) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idDetalleVenta] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Menu]    Script Date: 25/09/2025 9:28:59 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Menu](
+	[idMenu] [int] IDENTITY(1,1) NOT NULL,
+	[descripcion] [varchar](30) NULL,
+	[idMenuPadre] [int] NULL,
+	[icono] [varchar](30) NULL,
+	[controlador] [varchar](30) NULL,
+	[paginaAccion] [varchar](30) NULL,
+	[esActivo] [bit] NULL,
+	[fechaRegistro] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idMenu] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Negocio]    Script Date: 25/09/2025 9:28:59 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Negocio](
+	[idNegocio] [int] NOT NULL,
+	[urlLogo] [varchar](500) NULL,
+	[nombreLogo] [varchar](100) NULL,
+	[numeroDocumento] [varchar](50) NULL,
+	[nombre] [varchar](50) NULL,
+	[correo] [varchar](50) NULL,
+	[direccion] [varchar](50) NULL,
+	[telefono] [varchar](50) NULL,
+	[porcentajeImpuesto] [decimal](10, 2) NULL,
+	[simboloMoneda] [varchar](5) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idNegocio] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[NumeroCorrelativo]    Script Date: 25/09/2025 9:28:59 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[NumeroCorrelativo](
+	[idNumeroCorrelativo] [int] IDENTITY(1,1) NOT NULL,
+	[ultimoNumero] [int] NULL,
+	[cantidadDigitos] [int] NULL,
+	[gestion] [varchar](100) NULL,
+	[fechaActualizacion] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idNumeroCorrelativo] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Producto]    Script Date: 25/09/2025 9:28:59 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Producto](
+	[idProducto] [int] IDENTITY(1,1) NOT NULL,
+	[codigoBarra] [varchar](50) NULL,
+	[marca] [varchar](50) NULL,
+	[descripcion] [varchar](100) NULL,
+	[idCategoria] [int] NULL,
+	[stock] [int] NULL,
+	[urlImagen] [varchar](500) NULL,
+	[nombreImagen] [varchar](100) NULL,
+	[precio] [decimal](10, 2) NULL,
+	[esActivo] [bit] NULL,
+	[fechaRegistro] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idProducto] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Rol]    Script Date: 25/09/2025 9:28:59 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Rol](
+	[idRol] [int] IDENTITY(1,1) NOT NULL,
+	[descripcion] [varchar](30) NULL,
+	[esActivo] [bit] NULL,
+	[fechaRegistro] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idRol] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[RolMenu]    Script Date: 25/09/2025 9:28:59 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[RolMenu](
+	[idRolMenu] [int] IDENTITY(1,1) NOT NULL,
+	[idRol] [int] NULL,
+	[idMenu] [int] NULL,
+	[esActivo] [bit] NULL,
+	[fechaRegistro] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idRolMenu] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[TipoDocumentoVenta]    Script Date: 25/09/2025 9:28:59 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[TipoDocumentoVenta](
+	[idTipoDocumentoVenta] [int] IDENTITY(1,1) NOT NULL,
+	[descripcion] [varchar](50) NULL,
+	[esActivo] [bit] NULL,
+	[fechaRegistro] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idTipoDocumentoVenta] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Usuario]    Script Date: 25/09/2025 9:28:59 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Usuario](
+	[idUsuario] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [varchar](50) NULL,
+	[correo] [varchar](50) NULL,
+	[telefono] [varchar](50) NULL,
+	[idRol] [int] NULL,
+	[urlFoto] [varchar](max) NULL,
+	[nombreFoto] [varchar](100) NULL,
+	[clave] [varchar](100) NULL,
+	[esActivo] [bit] NULL,
+	[fechaRegistro] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idUsuario] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Venta]    Script Date: 25/09/2025 9:28:59 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Venta](
+	[idVenta] [int] IDENTITY(1,1) NOT NULL,
+	[numeroVenta] [varchar](6) NULL,
+	[idTipoDocumentoVenta] [int] NULL,
+	[idUsuario] [int] NULL,
+	[documentoCliente] [varchar](10) NULL,
+	[nombreCliente] [varchar](20) NULL,
+	[subTotal] [decimal](10, 2) NULL,
+	[impuestoTotal] [decimal](10, 2) NULL,
+	[Total] [decimal](10, 2) NULL,
+	[fechaRegistro] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[idVenta] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Categoria] ADD  DEFAULT (getdate()) FOR [fechaRegistro]
+GO
+ALTER TABLE [dbo].[Menu] ADD  DEFAULT (getdate()) FOR [fechaRegistro]
+GO
+ALTER TABLE [dbo].[Producto] ADD  DEFAULT (getdate()) FOR [fechaRegistro]
+GO
+ALTER TABLE [dbo].[Rol] ADD  DEFAULT (getdate()) FOR [fechaRegistro]
+GO
+ALTER TABLE [dbo].[RolMenu] ADD  DEFAULT (getdate()) FOR [fechaRegistro]
+GO
+ALTER TABLE [dbo].[TipoDocumentoVenta] ADD  DEFAULT (getdate()) FOR [fechaRegistro]
+GO
+ALTER TABLE [dbo].[Usuario] ADD  DEFAULT (getdate()) FOR [fechaRegistro]
+GO
+ALTER TABLE [dbo].[Venta] ADD  DEFAULT (getdate()) FOR [fechaRegistro]
+GO
+ALTER TABLE [dbo].[DetalleVenta]  WITH CHECK ADD FOREIGN KEY([idVenta])
+REFERENCES [dbo].[Venta] ([idVenta])
+GO
+ALTER TABLE [dbo].[Menu]  WITH CHECK ADD FOREIGN KEY([idMenuPadre])
+REFERENCES [dbo].[Menu] ([idMenu])
+GO
+ALTER TABLE [dbo].[Producto]  WITH CHECK ADD FOREIGN KEY([idCategoria])
+REFERENCES [dbo].[Categoria] ([idCategoria])
+GO
+ALTER TABLE [dbo].[RolMenu]  WITH CHECK ADD FOREIGN KEY([idMenu])
+REFERENCES [dbo].[Menu] ([idMenu])
+GO
+ALTER TABLE [dbo].[RolMenu]  WITH CHECK ADD FOREIGN KEY([idRol])
+REFERENCES [dbo].[Rol] ([idRol])
+GO
+ALTER TABLE [dbo].[Usuario]  WITH CHECK ADD FOREIGN KEY([idRol])
+REFERENCES [dbo].[Rol] ([idRol])
+GO
+ALTER TABLE [dbo].[Venta]  WITH CHECK ADD FOREIGN KEY([idTipoDocumentoVenta])
+REFERENCES [dbo].[TipoDocumentoVenta] ([idTipoDocumentoVenta])
+GO
+ALTER TABLE [dbo].[Venta]  WITH CHECK ADD FOREIGN KEY([idUsuario])
+REFERENCES [dbo].[Usuario] ([idUsuario])
+GO
+USE [master]
+GO
+ALTER DATABASE [DBVENTA] SET  READ_WRITE 
+GO
